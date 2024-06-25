3d5277f246fe663d8971476cc1f61c597156f11e
JeffersonAguirre0217 <jeffersonaguirre02@outlook.com>
2024-06-25 09:54:01 -0500

    FIX

--- src/components/products/addEditTemplateProduct.js
+++ src/components/products/addEditTemplateProduct.js
@@ -14,10 +14,11 @@ function AddEditProduct() {
     const { id } = useParams();
     const [title, setTitle] = useState();
     const [file, setFile] = useState(null);
+    const [base64, setBase64] = useState(null);
     const product = actionProducts.getById(id)
     const categories = storeApp(state => state.categories.list)
     let imgBase64Id = null
-    
+
     const styleOptions = {
         buttonAdd: 'file:bg-violet-500 file:hover:bg-violet-600 file:active:bg-violet-700 file:focus:outline-none file:focus:ring file:focus:ring-violet-300 file:text-white w-auto file:py-2  file:rounded-md',
         buttonSave: 'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 py-2 px-3 m-1  rounded-md',
@@ -26,7 +27,7 @@ function AddEditProduct() {
     }
 
     useEffect(() => {
-        
+
         if (id) {
             setTitle('Edit product');
             setValue('name', product.name,)
@@ -34,7 +35,7 @@ function AddEditProduct() {
             setValue('price', product.price)
             setValue('categoryDescription', product.categoryDescription)
             setValue('category', product.category)
-            
+
         } else {
             setTitle('Add product');
         }
@@ -67,10 +68,13 @@ function AddEditProduct() {
         //covertImgeToBase64(e.target.files[0])
         //debugger
     }
-    
+
     function covertImgeToBase64(newFile) {
         let imgs = JSON.parse(localStorage.getItem('imgTest')) || [];
-        let fileTemp = {};
+        let fileTemp = {
+            id: null,
+            img64: null
+        };
         let reader = new FileReader();
         reader.readAsDataURL(newFile);
         reader.onload = function () {
@@ -78,85 +82,89 @@ function AddEditProduct() {
             let auxiliar
             let temp
             auxiliar = reader.result;
-            if(auxiliar){
+            if (auxiliar) {
                 temp = auxiliar.split(',')
-                fileTemp.id = imgs.length ? Math.max(...imgs.map(x => x.id)) + 1 : 1;
+                //fileTemp.id = imgs.length ? Math.max(...imgs.map(x => x.id)) + 1 : 1;
                 fileTemp.img64 = temp[1]
-                imgs.push(fileTemp);
-                imgBase64Id = fileTemp.id
+                //setBase64(fileTemp);
+                //debugger
+                //imgs.push(fileTemp);
+                //imgBase64Id = fileTemp.id
                 //localStorage.setItem('imgTest', JSON.stringify(imgs)); 
             }
         };
-        
+        console.log('img', fileTemp)
+        return fileTemp
     }
 
     function onSubmit(data) {
-        //debugger
-        console.log('file', file)
-        console.log('imgBase64Id',imgBase64Id)
 
-        if(file){
+        if (file) {
             data.urlImg = covertImgeToBase64(file)
         }
-        // if (id) {
-        //     actionProducts.updateProduct(id, data)
-        // } else {
-        //     actionProducts.addNewProduct(data)
-        // }
 
-        //history.navigate('/products')
+        console.log('newData', data)
+
+        if (id) {
+            actionProducts.updateProduct(id, data)
+        } else {
+            actionProducts.addNewProduct(data)
+        }
+
+        // history.navigate('/products')
+
     }
 
     return (
         <div className='mt-3'>
 
             <h2>{title}</h2>
-            
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="row">
-                        <div className='mb-3 col-12'>
-                            <input type="file" className={styleOptions.buttonAdd} {...register('urlImg')} onChange={(healdfile)}></input>
-                            {file ? <img alt="Preview" height="60" src={URL.createObjectURL(file)} /> : null}
-                        </div>
-                        <div className="mb-3 col-12">
-                            <label className="form-label">Product</label>
-                            <input name="lastName" type="text" {...register('name')} className={`form-control ${errors.cant ? 'is-invalid' : ''}`} />
-                            <div className="invalid-feedback">{errors.name?.message}</div>
-                        </div>
-                        <div className="mb-3 col-4">
-                            <label className="form-label">Cant</label>
-                            <input name="lastName" type="text" {...register('cant')} className={`form-control ${errors.cant ? 'is-invalid' : ''}`} />
-                            <div className="invalid-feedback">{errors.cant?.message}</div>
-                        </div>
-                        <div className="mb-3 col-4">
-                            <label className="form-label">Price</label>
-                            <input name="lastName" type="text" {...register('price')} className={`form-control ${errors.price ? 'is-invalid' : ''}`} />
-                            <div className="invalid-feedback">{errors.price?.message}</div>
-                        </div>
-                        <div className="mb-3 col-4">
-                            <label className="form-label">Category</label><br></br>
-                            <select {...register("category")} className='selectroCategory'>
-                                {categories.map(category =>
-                                    <option key={category.id}>{category.name}</option>
-                                )}
-                            </select>
-                        </div>
-                        <div className="mb-3 col-12">
-                            <label className="form-label">description</label>
-                            <input name="lastName" type="text" {...register('categoryDescription')} className={`form-control ${errors.price ? 'is-invalid' : ''}`} />
-                            <div className="invalid-feedback">{errors.categoryDescription?.message}</div>
-                        </div>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="row">
+                    <div className='mb-3 col-12'>
+                        <input type="file" className={styleOptions.buttonAdd} {...register('urlImg')} onChange={(healdfile)}></input>
+                        {file ? <img alt="Preview" height="60" src={URL.createObjectURL(file)} /> : null}
                     </div>
-                    <div className="mb-3">
-                        <button type="submit" disabled={isSubmitting} className={styleOptions.buttonSave}>
-                            {isSubmitting && <span className="spinner-border spinner-border-sm me-1"></span>}
-                            Save
-                        </button>
-                        <button onClick={() => reset()} type="button" disabled={isSubmitting} className={styleOptions.buttonReset}>Reset</button>
-                        <Link to="/products"><button className={styleOptions.buttonBack}>Cancel</button></Link>
+                    <div className="mb-3 col-12">
+                        <label className="form-label">Product</label>
+                        <input name="lastName" type="text" {...register('name')} className={`form-control ${errors.cant ? 'is-invalid' : ''}`} />
+                        <div className="invalid-feedback">{errors.name?.message}</div>
                     </div>
-                </form>
-            
+                    <div className="mb-3 col-4">
+                        <label className="form-label">Cant</label>
+                        <input name="lastName" type="text" {...register('cant')} className={`form-control ${errors.cant ? 'is-invalid' : ''}`} />
+                        <div className="invalid-feedback">{errors.cant?.message}</div>
+                    </div>
+                    <div className="mb-3 col-4">
+                        <label className="form-label">Price</label>
+                        <input name="lastName" type="text" {...register('price')} className={`form-control ${errors.price ? 'is-invalid' : ''}`} />
+                        <div className="invalid-feedback">{errors.price?.message}</div>
+                    </div>
+                    <div className="mb-3 col-4">
+                        <label className="form-label">Category</label><br></br>
+                        <select {...register("category")} className='selectroCategory'>
+                            {categories.map(category =>
+                                <option key={category.id}>{category.name}</option>
+                            )}
+                        </select>
+                    </div>
+                    <div className="mb-3 col-12">
+                        <label className="form-label">description</label>
+                        <input name="lastName" type="text" {...register('categoryDescription')} className={`form-control ${errors.price ? 'is-invalid' : ''}`} />
+                        <div className="invalid-feedback">{errors.categoryDescription?.message}</div>
+                    </div>
+                </div>
+                <div className="mb-3">
+                    <button type="submit" disabled={isSubmitting} className={styleOptions.buttonSave}>
+                        {isSubmitting && <span className="spinner-border spinner-border-sm me-1"></span>}
+                        Save
+                    </button>
+                    <button onClick={() => reset()} type="button" disabled={isSubmitting} className={styleOptions.buttonReset}>Reset</button>
+                    <Link to="/products"><button className={styleOptions.buttonBack}>Cancel</button></Link>
+                </div>
+            </form>
+
             {/* {
                 <div className="text-center m-5">
                     <span className="spinner-border spinner-border-lg align-center"></span>
